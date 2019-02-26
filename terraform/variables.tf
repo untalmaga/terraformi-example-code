55a415d055bbd6b85b79f70a60c0a16bd5e3c537
Fernando Magallanes <fernando.magallanes@digitalonus.com>
2019-02-26 15:51:56 -0600

    first commit

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "aws-region" {
+    default = "us-west-2"
+} 
+
+variable "ami-id" {
+    default = "ami-0bbe6b35405ecebdb"
+}
+
+variable "aws-key-pair" {
+    default = "fernando-work"
+}
+
+
