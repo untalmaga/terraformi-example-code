1a6d2d70419bb9addaeadb147de2b41e909b21b6
Fernando Magallanes <fernando.magallanes@digitalonus.com>
2019-02-26 16:30:09 -0600

    empty variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,9 +2,9 @@ variable "aws-region" {
     default = "us-west-2"
 } 
 
-variable "aws_access_key" {
-    default = ""
-}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
 variable "ami-id" {
     default = "ami-0bbe6b35405ecebdb"
 }
