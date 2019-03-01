a9c0f3a4e9c2cb1a1941f4782951fddb2a40dc89
Fernando Magallanes <fernando.magallanes@digitalonus.com>
2019-02-28 19:41:50 -0600

    ...

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ resource "aws_instance" "cool-website-instance" {
       "sudo apt-get -y install software-properties-common",
       "sudo apt-add-repository --yes --update ppa:ansible/ansible",
       "sudo apt-get -y install ansible git", 
-      "sudo git clone https://github.com/untalmaga/ansible-terraformi.git /tmp",
+      "sudo git clone https://github.com/untalmaga/ansible-terraformi.git /tmp/",
       "sudo ansible-playbook /tmp/playbook.yml"
     ]
   }
